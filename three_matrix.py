1624ac122fccf8608efa9e7b1aae502d719b9ff8
H.Yang <h.yang1@student.tue.nl>
2024-04-30 00:23:36 +0200

    Some trials to find the reason why the parameters don't update

--- three_matrix.py
+++ three_matrix.py
@@ -31,25 +31,36 @@ class FCN(nn.Module):
         x = self.fce(x)
         return x
     
-def plotdataphysics(x_data,t_data,x_physics,t_physics):
-    # visualize collocation points for 2D input space (x, t)
-    plt.figure()
-    plt.scatter(x_data.detach().numpy(), t_data.detach().numpy(),s=4., c='blue', marker='o', label='Data points')
-    plt.scatter(x_physics.detach().numpy(), t_physics.detach().numpy(),s=4., c='red', marker='o', label='Physics points')
-    plt.title('Samples of the PDE solution y(x,t) for training')
-    plt.xlim(0., 2.)
-    plt.ylim(0., 1.)
-    plt.grid(True)
-    plt.xlabel('x')
-    plt.ylabel('t')
-    plt.legend('Data points and physics points')
-    plt.show()
-    plt.show(block=True)
 
+class FCN_2output(nn.Module):
+    "Defines a standard fully-connected network in PyTorch"
+    
+    def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
+        super().__init__()
+        activation = nn.Tanh
+        self.fcs = nn.Sequential(*[
+                        nn.Linear(N_INPUT, N_HIDDEN),
+                        activation()])
+        self.fch = nn.Sequential(*[
+                        nn.Sequential(*[
+                            nn.Linear(N_HIDDEN, N_HIDDEN),
+                            activation()]) for _ in range(N_LAYERS-1)])
+        self.fce1 = nn.Linear(N_HIDDEN, N_OUTPUT)
+        self.fce2 = nn.Linear(N_HIDDEN, N_OUTPUT)
 
-torch.manual_seed(1234)
+    def forward(self, input):
+        x = torch.cat(input, dim=-1)
+        x = self.fcs(x)
+        x = self.fch(x)
+        u1 = self.fce1(x)
+        u2 = self.fce2(x)
+        return u1,u2
+    
+
+# torch.manual_seed(1234)
+torch.manual_seed(123)
 # first, create some noisy observational data
-file = './dataset/2matrix_2-5_3-6.mat'
+file = './dataset/3matrix_2-5_3-6_4-7.mat'
 x,t,u1,u2 = dataprocessing.load_matrix(file)
 X,T,U1,U2 = dataprocessing.matrix_totensor(x,t,u1,u2)
 X_test,T_test,U1_test,U2_test = dataprocessing.reshape_matrix(X,T,U1,U2)
@@ -84,23 +95,32 @@ T_physics_tensor.requires_grad = True
 
 # define a neural network to train
 pinn = FCN(2,2,32,2)
+# pinn = FCN_2output(2,1,32,2)
 
-alpha = torch.nn.Parameter(torch.randn(2), requires_grad=True)
-beta = torch.nn.Parameter(torch.randn(2), requires_grad=True)
-# gamma = torch.nn.Parameter(torch.randn(2), requires_grad=True)
-
+# alpha1 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+# beta1 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+# gamma1 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+# alpha2 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+# beta2 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+# gamma2 = torch.nn.Parameter(torch.ones(1, requires_grad=True))
+alpha1 = torch.nn.Parameter(torch.tensor(1.0), requires_grad=True)
+beta1 = torch.nn.Parameter(torch.tensor(1.0),requires_grad=True)
+gamma1 = torch.nn.Parameter(torch.tensor(1.0), requires_grad=True)
+alpha2 = torch.nn.Parameter(torch.tensor(4.0),requires_grad=True)
+beta2 = torch.nn.Parameter(torch.tensor(5.0), requires_grad=True)
+gamma2 = torch.nn.Parameter(torch.tensor(6.0),requires_grad=True)
 
 alps1 = []
 alps2 = []
 bets1 = []
 bets2 = []
-# gams1 = []
-# gams2 = []
+gams1 = []
+gams2 = []
 
 
 # add mu to the optimiser
 # TODO: write code here
-optimiser = torch.optim.Adam(list(pinn.parameters())+[alpha]+[beta],lr=1e-3)
+optimiser = torch.optim.Adam(list(pinn.parameters())+[alpha1]+[beta1]+[gamma1]+[alpha2]+[beta2]+[gamma2],lr=0.001)
 writer = SummaryWriter()
 
 theta = 0.01
@@ -110,7 +130,7 @@ i = 0
 time_start = time.time()
 
 while loss > theta:
-# for i in range(10001):
+# for i in range(40001):
     
     optimiser.zero_grad()
     
@@ -130,8 +150,9 @@ while loss > theta:
     du2dx = torch.autograd.grad(physic_output2, X_physics_tensor, torch.ones_like(physic_output2), create_graph=True)[0]
     d2u1dx2 = torch.autograd.grad(du1dx, X_physics_tensor, torch.ones_like(du1dx), create_graph=True)[0]
     d2u2dx2 = torch.autograd.grad(du2dx, X_physics_tensor, torch.ones_like(du2dx), create_graph=True)[0]
-    # loss1 = torch.mean((alpha[0]*d2u1dx2+beta[0]*du1dx+gamma[0]*physic_output1-du1dt)**2+(alpha[1]*d2u2dx2+beta[1]*du2dx+gamma[1]*physic_output2-du2dt)**2)
-    loss1 = torch.mean((alpha[0]*d2u1dx2+beta[0]*du1dx-du1dt)**2+(alpha[1]*d2u2dx2+beta[1]*du2dx-du2dt)**2)
+    # loss1 = torch.mean((alpha1*d2u1dx2+beta1*du1dx+gamma1*physic_output1-du1dt)**2+(alpha2*d2u2dx2+beta2*du2dx+gamma2*physic_output2-du2dt)**2)
+    # loss1 = torch.mean((alpha[0]*d2u1dx2+beta[0]*du1dx-du1dt)**2+(alpha[1]*d2u2dx2+beta[1]*du2dx-du2dt)**2)
+    loss1 = torch.mean(((alpha1*d2u1dx2+beta1*du1dx+gamma1*physic_output1-du1dt)+(alpha2*d2u2dx2+beta2*du2dx+gamma2*physic_output2-du2dt))**2)
     writer.add_scalar('loss1',loss1,i)
     
     # compute data loss
@@ -140,7 +161,7 @@ while loss > theta:
     data_output = pinn(data_input)
     data_output1 = data_output[:, 0].view(-1, 1)
     data_output2 = data_output[:, 1].view(-1, 1)
-    loss2 = torch.mean((U1_data_tensor - data_output1)**2+(U2_data_tensor-data_output2)**2)
+    loss2 = torch.mean(((U1_data_tensor - data_output1)+(U2_data_tensor-data_output2))**2)
     writer.add_scalar('loss2',loss2,i)
 
     # backpropagate joint loss, take optimiser step
@@ -150,19 +171,19 @@ while loss > theta:
     
     # record mu value
     # TODO: write code here
-    alps1.append(alpha[0].item())
-    alps2.append(alpha[1].item())
-    bets1.append(beta[0].item())
-    bets2.append(beta[1].item())
-    # gams1.append(gamma[0].item())
-    # gams2.append(gamma[1].item())
+    alps1.append(alpha1.item())
+    alps2.append(alpha2.item())
+    bets1.append(beta1.item())
+    bets2.append(beta2.item())
+    gams1.append(gamma1.item())
+    gams2.append(gamma2.item())
     writer.add_scalar('train_loss',loss,i)
-    writer.add_scalar('alpha1',alpha[0],i)
-    writer.add_scalar('alpha2',alpha[1],i)
-    writer.add_scalar('beta1',beta[0],i)
-    writer.add_scalar('beta2',beta[1],i)
-    # writer.add_scalar('gamma1',gamma[0],i)
-    # writer.add_scalar('gamma2',gamma[1],i)
+    writer.add_scalar('alpha1',alpha1,i)
+    writer.add_scalar('alpha2',alpha2,i)
+    writer.add_scalar('beta1',beta1,i)
+    writer.add_scalar('beta2',beta2,i)
+    writer.add_scalar('gamma1',gamma1,i)
+    writer.add_scalar('gamma2',gamma2,i)
     # plot the result as training progresses
     if i % 500 == 0: 
         # u = pinn(t_test).detach()
@@ -172,11 +193,11 @@ while loss > theta:
         # plt.title(f"Training step {i}")
         # plt.legend()
         # plt.show()
-        print(f'epoch: {i}  train loss :{loss}' )
+        print(f'epoch: {i}  train loss :{loss}, alpha: {alpha1.item(),alpha2.item()},beta:{beta1.item(),beta2.item()},gamma:{gamma1.item(),gamma2.item()}' )
     i = i+1
 
         
-torch.save(pinn,"./model/18060424.pkl.")
+torch.save(pinn,"./model/20010426.pkl.")
 time_end = time.time()
 time_sum = time_end - time_start
 print('训练时间 {:.0f}分 {:.0f}秒'.format(time_sum // 60, time_sum % 60))
@@ -213,21 +234,21 @@ plt.legend()
 plt.xlabel("Training step")
 plt.show()
 
-# plt.figure()
-# plt.title("gamma_1")
-# plt.plot(gams1, label="PINN estimate")
-# plt.hlines(7, 0, len(gams1), label="True value", color="tab:green")
-# plt.legend()
-# plt.xlabel("Training step")
-# plt.show()
+plt.figure()
+plt.title("gamma_1")
+plt.plot(gams1, label="PINN estimate")
+plt.hlines(4, 0, len(gams1), label="True value", color="tab:green")
+plt.legend()
+plt.xlabel("Training step")
+plt.show()
 
-# plt.figure()
-# plt.title("gamma_2")
-# plt.plot(gams2, label="PINN estimate")
-# plt.hlines(7, 0, len(gams2), label="True value", color="tab:green")
-# plt.legend()
-# plt.xlabel("Training step")
-# plt.show()
+plt.figure()
+plt.title("gamma_2")
+plt.plot(gams2, label="PINN estimate")
+plt.hlines(7, 0, len(gams2), label="True value", color="tab:green")
+plt.legend()
+plt.xlabel("Training step")
+plt.show()
 
 # plt.figure()
 # plt.title("gamma")
